3d43defc2a86448f968e9ef43984381bd57eb672
claudiney63 <claudineyryan63@gmail.com>
2022-05-04 22:26:41 -0300

    inicio dos estudos

---
+++ estudo_basico/hello_world.js
@@ -0,0 +1,27 @@
+// alert('Hello World')
+
+/* console.log(
+    document.getElementById("titulo").innerHTML
+) */
+
+/* console.log (
+    navigator.appCodeName
+) */
+
+/* function voltar() {
+    history.back()
+}
+
+function seguir() {
+    //location.href = "https://www.google.com.br"
+    history.go()
+} */
+
+// document.getElementById("larg").innerHTML = screen.height
+
+/* console.log (
+    document.getElementsByClassName("car")[0].innerHTML,
+    document.getElementsByTagName("div"), //pega pela tag
+    document.querySelector("#div"), //pega o primeiro elemento
+    document.querySelector(".car")
+) */
