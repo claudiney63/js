481d382b2e983d83c220134695ae6ddaeeec5757
claudiney63 <claudineyryan63@gmail.com>
2022-06-02 19:57:20 -0300

    alteração nos parametros da função imc

--- estudo_basico/#0 desafios/IMC (desafio 2)/script.js
+++ estudo_basico/#0 desafios/IMC (desafio 2)/script.js
@@ -1,19 +1,30 @@
-function imc(peso, altura) {
-    return peso / (altura ** 2)
+function imc(peso, altura, callback) {
+    var imc =  peso / (altura ** 2)
+
+    if(callback && typeof callback === 'function') {
+        return callback(imc)
+    }
+    
+    return imc
 }
 
 function tipoCorporeo(imc) {
 
     if (imc < 18.5)
-        console.log("ABAIXO DO PESO")
+        return "ABAIXO DO PESO"
     else if (imc >= 18.5 && imc <= 24.9)
-        console.log("PESO NORMAL")
+        return "PESO NORMAL"
     else if (imc >= 25 && imc <= 29.9)
-        console.log("SOBREPESO")
+        return "SOBREPESO"
     else if (imc >= 30 && imc <= 34.9)
-        console.log("OBESIDADE GRAU 1")
+        return "OBESIDADE GRAU 1"
     else if (imc >= 35 && imc <= 39.9)
-        console.log("OBESIDADE GRAU 2")
+        return "OBESIDADE GRAU 2"
     else if (imc > 40)
-        console.log("OBESIDADE GRAU 3")
+        return "OBESIDADE GRAU 3"
 }
+
+var peso = 61
+var altura = 1.76
+
+console.log('imc: ', imc(peso, altura, tipoCorporeo))
